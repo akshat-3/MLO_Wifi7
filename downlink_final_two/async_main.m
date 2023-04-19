2404e3d82a315bc93c8bc29ff11d5255b90a0ea3
akshat-3 <82972138+akshat-3@users.noreply.github.com>
2023-04-19 22:38:22 +0530

    Update async_main.m

--- downlink_final_two/async_main.m
+++ downlink_final_two/async_main.m
@@ -425,7 +425,7 @@ for s=(historical_samples_req+1):num_samples   %the iterator s accounts for hist
         [ap.interface_two, sta(i).interface_two] = update_interface_status_STA(ap.interface_two, num_samples, sample_no, sta(i).interface_two, rssi_matrix(k, :), occupancy_matrix(k, :), true, occupancy_at_access);
     end
 
-    if(k = 1000)
+    if(mod(k,1000) == 0)
         fprintf(file, "\n%d bits", ap.interface_one.num_data_bits_sent);
         fprintf(file, "\n%d bits", ap.interface_two.num_data_bits_sent);
         fprintf(file, "\n%d bits", sta(sta_no).interface_one.num_data_bits_received);
