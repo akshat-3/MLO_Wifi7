d5eee0e3df0cd5ea3fc0bad1964b3aa52324887b
akshat-3 <82972138+akshat-3@users.noreply.github.com>
2023-05-10 10:15:13 +0530

    third

--- downlink_final_two/async_main.m
+++ downlink_final_two/async_main.m
@@ -469,19 +469,19 @@ for s=(historical_samples_req+1):num_samples   %the iterator s accounts for hist
         end
     end
 
-    if count1 == 1 && ~(ap.interface_one.state == 3)
+    if count1 == 1 && ~(ap.interface_one.state == 3) && ~(ap.interface_one.state == 5)
         ap.interface_one.previous_state = ap.interface_one.state;
         ap.interface_one.state = 5;
     end
 
-    if count2 == 1 && ~(ap.interface_two.state == 3)
+    if count2 == 1 && ~(ap.interface_two.state == 3) && ~(ap.interface_two.state == 5)
         ap.interface_two.previous_state = ap.interface_two.state;
         ap.interface_two.state = 5;
     end
 
     if count1 == 1 && ap.interface_one.state == 3
         for i = 1:n_sta
-            if sta(i).interface_one.state == 3
+            if sta(i).interface_one.state == 3 && ~(sta(i).interface_one.state == 5)
                 sta(i).interface_one.previous_state = sta(i).interface_one.state;
                 sta(i).interface_one.state = 5;
             end
@@ -490,7 +490,7 @@ for s=(historical_samples_req+1):num_samples   %the iterator s accounts for hist
 
     if count2 == 1 && ap.interface_two.state == 3
         for i = 1:n_sta
-            if sta(i).interface_two.state == 3
+            if sta(i).interface_two.state == 3 && ~(sta(i).interface_two.state == 5)
                 sta(i).interface_two.previous_state = sta(i).interface_two.state;
                 sta(i).interface_two.state = 5;
             end
